73b4e5f36ccbd148bd41d7a92152493af6fe4ffa
Mohit Narayan Shah <mohit_2051@yahoo.com>
2022-02-24 12:59:27 +0000

    New program Amortizing Engine development in progress

---
+++ Amortizing Engine/AmortizingEngine.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    const int monthsInYear = 12;
+    double principal = 0.0, realInterest =0.0, interest = 0.0;
+    int yearsOfLoan = 0;
+    cout<<"Enter the principal amount: \n";
+    cin>> principal;
+    cout<<"Enter the interest: \n";
+    cin>> interest;
+    cout<< "Enter the years of loan: \n";
+    cin>> yearsOfLoan;
+
+    double monthlyInterest = (double)interest/monthsInYear;
+    long monthsOfLoan = yearsOfLoan * monthsInYear;
+
+    cout<< "principal = " << principal <<" interest = "<<interest<<" yearsofLoan = " << yearsOfLoan <<" monthlyInterest = " <<monthlyInterest<< " monthsOfLoan = "<<monthsOfLoan<<endl;
+
+    return 0;
+
+}
